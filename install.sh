c84a99869225bf0efe4415063f398f8f0fe93637
Daniel <daniel.garcia@opinno.com>
2018-10-04 10:00:26 +0200

    Fixed bugs

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 dpkg-query -l python3-tk || sudo apt-get install python3-tk
+printf "#! /bin/bash\nsudo python3 `pwd`/toggle_hosts.py &\n" > toggle_hosts
+chmod +x toggle_hosts
 sudo cp toggle_hosts /usr/local/bin
 toggle_hosts
