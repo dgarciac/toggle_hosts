767591655ae4d8a07ad5c07b80d8036efc3e196b
Mr-Programs Erick Lestrange <livingandwasting@hotmail.com>
2019-02-15 12:49:54 +0100

    tk dependency fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-dpkg-query -l python3-tk || sudo apt-get install python3-tk
+dpkg -s python3-tk 2>/dev/null >/dev/null || sudo apt-get -y install python3-tk
 printf "#! /bin/bash\nsudo python3 `pwd`/toggle_hosts.py &\n" > toggle_hosts
 chmod +x toggle_hosts
 sudo cp toggle_hosts /usr/local/bin
